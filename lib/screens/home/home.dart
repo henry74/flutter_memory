c74a7ac8c0466a8ab64268ac9c03bbf0ec4aba54
Henry Hwangbo <henry74@gmail.com>
2020-03-28 16:01:42 -0500

    Completed game

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -1,21 +1,154 @@
+import 'dart:async';
+
+import 'package:flip_card/flip_card.dart';
 import 'package:flutter/material.dart';
 
-class Home extends StatelessWidget {
+int level = 8;
+
+class Home extends StatefulWidget {
+  final int size;
+  const Home({Key key, this.size = 8}) : super(key: key);
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  List<GlobalKey<FlipCardState>> cardStateKeys = [];
+  List<bool> cardFlips = [];
+  List<String> data = [];
+  int previousIndex = -1;
+  bool flip = false;
+
+  int time = 0;
+  Timer timer;
+
+  @override
+  void initState() {
+    super.initState();
+    for (var i = 0; i < widget.size; i++) {
+      cardStateKeys.add(GlobalKey<FlipCardState>());
+      cardFlips.add(true);
+    }
+    for (var i = 0; i < widget.size ~/ 2; i++) {
+      data.add(i.toString());
+    }
+    for (var i = 0; i < widget.size ~/ 2; i++) {
+      data.add(i.toString());
+    }
+    startTimer();
+    data.shuffle();
+  }
+
+  startTimer() {
+    timer = Timer.periodic(Duration(seconds: 1), (t) {
+      setState(() {
+        time = time + 1;
+      });
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.black,
-      appBar: AppBar(
-        title: Text('My Memory Game'),
-      ),
-      body: Column(
-        children: <Widget>[
-          Container(
-            height: 100.0,
-            color: Theme.of(context).accentColor,
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Column(
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Text(
+                  "$time",
+                  style: Theme.of(context).textTheme.display2,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: GridView.builder(
+                  shrinkWrap: true,
+                  physics: NeverScrollableScrollPhysics(),
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 4,
+                  ),
+                  itemBuilder: (context, index) => FlipCard(
+                    key: cardStateKeys[index],
+                    onFlip: () {
+                      if (!flip) {
+                        flip = true;
+                        previousIndex = index;
+                      } else {
+                        flip = false;
+                        if (previousIndex != index) {
+                          if (data[previousIndex] != data[index]) {
+                            cardStateKeys[previousIndex]
+                                .currentState
+                                .toggleCard();
+                            previousIndex = index;
+                          } else {
+                            cardFlips[previousIndex] = false;
+                            cardFlips[index] = false;
+                            print(cardFlips);
+                            if (cardFlips.every((t) => t == false)) {
+                              print("Won");
+                              showResult();
+                            }
+                          }
+                        }
+                      }
+                    },
+                    direction: FlipDirection.HORIZONTAL,
+                    flipOnTouch: true,
+                    front: Container(
+                      margin: EdgeInsets.all(4.0),
+                      color: Colors.deepOrange[100],
+                    ),
+                    back: Container(
+                      margin: EdgeInsets.all(4.0),
+                      color: Colors.deepOrange,
+                      child: Center(
+                        child: Text(
+                          "${data[index]}",
+                          style: Theme.of(context).textTheme.display2,
+                        ),
+                      ),
+                    ),
+                  ),
+                  itemCount: data.length,
+                ),
+              )
+            ],
           ),
-          Expanded(
-            child: Container(color: Theme.of(context).backgroundColor),
+        ),
+      ),
+    );
+  }
+
+  showResult() {
+    showDialog(
+      context: context,
+      barrierDismissible: false,
+      builder: (context) => AlertDialog(
+        title: Text(
+          "Won!!!",
+          style: Theme.of(context).textTheme.display2,
+        ),
+        content: Text("Time $time"),
+        actions: <Widget>[
+          FlatButton(
+            onPressed: () {
+              Navigator.of(context).pushReplacement(
+                MaterialPageRoute(
+                  builder: (context) => Home(
+                    size: level,
+                  ),
+                ),
+              );
+              if (level >= 28) {
+                level = 8;
+              } else {
+                level += 4;
+              }
+            },
+            child: Text("NEXT"),
           )
         ],
       ),
